15f6438e42110a632cb3a741408ab4c4c8583afd
R3TH PRIV <119117781+R3TH-PRIV@users.noreply.github.com>
2023-10-23 21:20:58 +0800

    Delete OtherScripts/Blade Ball Anti-Cheat Bypass.lua

--- OtherScripts/Blade Ball Anti-Cheat Bypass.lua
+++
@@ -1,8 +0,0 @@
-for k,v in pairs(getgc(true)) do if pcall(function() return rawget(v,"indexInstance") end) and type(rawget(v,"indexInstance")) == "table" and (rawget(v,"indexInstance"))[1] == "kick" then v.tvk = {"kick",function() return game.Workspace:WaitForChild("") end} end end
-task.wait()
-
-game:GetService('StarterGui'):SetCore('SendNotification', {
-   Title = "R3TH PRIV",
-   Text = "ANTI KICK ENABLED",
-   Duration = 5,
-})
