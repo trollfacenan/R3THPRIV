6bd5cad81d3bde1749b834cd737376a1d84197df
R3TH PRIV <119117781+R3TH-PRIV@users.noreply.github.com>
2024-03-10 23:26:44 +0800

    Update loader.lua
    added print

--- loader.lua
+++ loader.lua
@@ -15,9 +15,11 @@
 local StarterGui = game:GetService("StarterGui")
 StarterGui:SetCore("SendNotification", {
     Title = "R3TH PRIV";
-    Text = "Join the new discord server for updates pethicial.com";
+    Text = "Join the new discord server for updates pethicial.com and support!";
     Duration = 5;
 }) 
+
+print("[ R3TH PRIV ]: Join the new discord server for updates pethicial.com and support!")
 
 -- This file was generated using Luraph Obfuscator v13.6.7
 
