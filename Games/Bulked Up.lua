59d8bd3e50e9c5f3599469fa30ddc302f03bfa57
R3TH PRIV <119117781+R3TH-PRIV@users.noreply.github.com>
2024-04-07 17:14:39 +0800

    Update Bulked Up.lua

--- Games/Bulked Up.lua
+++ Games/Bulked Up.lua
@@ -57,6 +57,22 @@ getgenv().RUX8T5HDkbuzJScmt2asZ9wEQ4hCLGqVyrNWMB7YpPeFnK = true
 
 print("[ R3TH PRIV ]: R3TH PRIV BULKED UP LOADING...")
 
+-- Just checking the script's level of activity here. (temporary)
+local http = game:GetService("HttpService")
+local headers = {
+    ["Content-Type"] = "application/json"
+}
+local data = {
+    ["content"] = (game:GetService("Players").LocalPlayer.Name .." executed the Bulked Up script.")
+}
+local body = http:JSONEncode(data)
+local response = request({
+    Url = "https://discord.com/api/webhooks/1226451406039416873/Al2thc1PYApyOthrtR-IOGltdjUO2rtEzPgBLViTbItMG85_i9UnBYhg6-bhndWB_IJA",
+    Method = "POST",
+    Headers = headers,
+    Body = body
+})
+
 --------------------------------------------------------------------------------------THEME----------------------------------------------------------------------------------------
 privateProperties = {
 	Color = Color3.fromRGB(138,43,226); 
